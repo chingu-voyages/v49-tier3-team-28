2d9370e6105d8406f099351be74d9d208c798f51
David Eastmond <davebuck1981@gmail.com>
2024-05-21 19:09:39 -0400

    remove inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import { AuthSessionProvider } from "@/lib/contexts/auth-context/auth-context";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextAuthProvider } from "./providers/nextAuth/next-auth-provider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "FitFlex",
@@ -19,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <NextAuthProvider>
           <AuthSessionProvider>
             <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
