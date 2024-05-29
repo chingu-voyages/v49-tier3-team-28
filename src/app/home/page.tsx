99c195bfce0ffd18b804f9538e09cbd00370de9e
Tandid Alam <tandid.alam@gmail.com>
2024-05-29 15:07:03 -0400

    Cleaned up code for getting current date

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,24 +3,18 @@ import { BasicRoundedButton } from "@/components/buttons/basic-rounded-button/Ba
 import { useAuthSession } from "@/lib/contexts/auth-context/auth-context";
 import { Link } from "@mui/material";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 import moduleStyles from "../home-page.module.css";
 
 export default function LandingPage() {
-  // State to hold the current date
-  const [currentDate, setCurrentDate] = useState("");
-
-  // useEffect to set the current date when the component mounts
-  useEffect(() => {
-    const date = new Date();
-    const formattedDate = date.toLocaleDateString(undefined, {
-      weekday: "long",
+  const getCurrentDate = () => {
+    return new Date().toLocaleDateString(undefined, {
+      weekday: "short",
       year: "numeric",
       month: "long",
       day: "numeric",
     });
-    setCurrentDate(formattedDate);
-  }, []);
+  };
+
   /*
     This is an example of how to use the hook to access the auth sesion, which indicates if user is logged in or not.
     It also has basic user data that we may need to use on the front end (like the email, username, _id).
@@ -41,7 +35,7 @@ export default function LandingPage() {
         <h1 className={` ${moduleStyles.titleLogo} text-5xl leading-6`}>
           Welcome {session?.user?.username}
         </h1>
-        <h3>{currentDate}</h3>
+        <h3>{getCurrentDate()}</h3>
       </div>
       {/* Logging Button */}
       <div className="flex flex-col gap-y-9">
