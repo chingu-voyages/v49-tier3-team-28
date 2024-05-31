6992bd7939686f01b7dadc95615f803cfd74a6a0
David Eastmond <davebuck1981@gmail.com>
2024-05-30 21:08:07 -0400

    install dayjs - fix folder structure

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -2,17 +2,12 @@
 import { BasicRoundedButton } from "@/components/buttons/basic-rounded-button/Basic-rounded-button";
 import { useAuthSession } from "@/lib/contexts/auth-context/auth-context";
 import { Link } from "@mui/material";
+import dayjs from "dayjs";
 import { useRouter } from "next/navigation";
-import moduleStyles from "../home-page.module.css";
 
 export default function LandingPage() {
   const getCurrentDate = () => {
-    return new Date().toLocaleDateString(undefined, {
-      weekday: "short",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    });
+    return dayjs().format("ddd, MMMM D, YYYY");
   };
 
   /*
@@ -32,7 +27,7 @@ export default function LandingPage() {
     <div className="flex flex-col items-center gap-y-36 justify-center w-screen h-screen bg-cover bg-no-repeat bg-center">
       {/* Header */}
       <div className="flex flex-col gap-y-10">
-        <h1 className={` ${moduleStyles.titleLogo} text-5xl leading-6`}>
+        <h1 className={`text-5xl leading-6`}>
           Welcome {session?.user?.username}
         </h1>
         <h3>{getCurrentDate()}</h3>
@@ -40,16 +35,14 @@ export default function LandingPage() {
       {/* Logging Button */}
       <div className="flex flex-col gap-y-9">
         <div>
-          <Link href="/createlog">
+          <Link href="/user/createlog">
             <BasicRoundedButton label="Start Logging" />
           </Link>
         </div>
       </div>
       {/* Metrics */}
       <div className="flex flex-col gap-y-9">
-        <h1 className={` ${moduleStyles.titleLogo} text-5xl leading-6`}>
-          Metrics
-        </h1>
+        <h1 className={`text-5xl leading-6`}>Metrics</h1>
         <div className="flex gap-x-5">
           <div>Logging Journal</div>
           <div>Favorite Exercise</div>
