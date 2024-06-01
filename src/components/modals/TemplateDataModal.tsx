d86be461179e5fb7ad04c0730d6ccaba95cc2c31
Tandid Alam <tandid.alam@gmail.com>
2024-06-01 18:57:00 -0400

    styling for Template Data Modal

--- src/components/modals/TemplateDataModal.tsx
+++ src/components/modals/TemplateDataModal.tsx
@@ -7,7 +7,7 @@ import { BasicRoundedButton } from "../buttons/basic-rounded-button/Basic-rounde
 interface TemplateDataModalProps {
   open: boolean;
   onClose: () => void;
-  exerciseData: ExerciseActivity;
+  exerciseData: ExerciseActivity[];
 }
 
 const TemplateDataModal: React.FC<TemplateDataModalProps> = ({
@@ -24,27 +24,44 @@ const TemplateDataModal: React.FC<TemplateDataModalProps> = ({
       <div className="flex flex-col w-1/2 h-3/4 bg-white p-10 rounded-xl relative justify-evenly items-center text-center">
         {/* Templates to Choose From */}
         {exerciseData !== null ? (
-          <div className="mt-4">
+          <div className="mt-4 w-full">
             <button className="absolute top-2 right-2" onClick={onClose}>
               <FiX />
             </button>
-            <h2 className="text-2xl font-semibold">Exercises</h2>
-            {exerciseData.map((exercise, eIdx) => (
-              <div key={eIdx} className="mt-2">
-                <h3 className="text-xl font-medium">{exercise.exerciseName}</h3>
-                {exercise.sets.map((set, sIdx) => (
-                  <div key={sIdx} className="text-left ml-4">
-                    <p>
-                      Set {set.setNumber}: {set.reps} reps, {set.weight}{" "}
-                      {set.unit}
-                    </p>
-                  </div>
-                ))}
-              </div>
-            ))}
+
+            <h2 className="text-2xl font-semibold mb-4">Exercise List</h2>
+
+            {/* Exercise Activity */}
+            <div className="min-h-96 max-h-96 overflow-y-auto overflow-hidden">
+              {exerciseData.map((exercise, eIdx) => (
+                <div className="rounded-xl mb-4 border border-gray-100 shadow-md relative">
+                  <h4 className="text-black font-bold p-2 text-center ">
+                    {exercise.exerciseName}
+                  </h4>
+                  <table className="w-full border-collapse bg-white">
+                    <thead>
+                      <tr className="text-white text-center bg-orange-500">
+                        <th className="p-left-2 font-bold">Set</th>
+                        <th className="p-2">Reps</th>
+                        <th className="p-2">Weight</th>
+                        <th className="p-2">Unit</th>
+                      </tr>
+                    </thead>
+                    {exercise.sets.map((set, eIdx) => (
+                      <tbody>
+                        <td className="p-2 text-center">{set.setNumber}</td>
+                        <td className="p-2 text-center">{set.reps}</td>
+                        <td className="p-2 text-center">{set.weight}</td>
+                        <td className="p-2 text-center">{set.unit}</td>
+                      </tbody>
+                    ))}
+                  </table>
+                </div>
+              ))}
+            </div>
           </div>
         ) : null}
-        <div className="flex flex-col justify-between h-28">
+        <div className="flex flex-col justify-between h-28 mt-4">
           <BasicRoundedButton onClick={onClose} label="Back to Selection" />
         </div>
       </div>
