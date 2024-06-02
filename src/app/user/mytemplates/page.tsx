715aed0aab4878ebb7e583d6fcd86709b714dc9e
Tandid Alam <tandid.alam@gmail.com>
2024-06-02 00:56:04 -0400

    created my templates page along with cards

---
+++ src/app/user/mytemplates/page.tsx
@@ -0,0 +1,130 @@
+import { BasicRoundedButton } from "@/components/buttons/basic-rounded-button/Basic-rounded-button";
+import TemplateCard from "@/components/cards/TemplateCard";
+import React from "react";
+
+interface MyTemplatesProps {}
+
+//TODO: remove this mock data once we have backend support
+let mockData = {
+  userId: "665291224e3640a167a73ae5",
+  logs: [
+    {
+      date: "2024-05-31T20:04:02.485Z",
+      name: "Leg Day",
+      exercises: [
+        {
+          exerciseName: "Walking Lunge",
+          sets: [
+            { setNumber: 1, weight: 0, unit: "lbs", reps: 8 },
+            { setNumber: 2, weight: 2, unit: "lbs", reps: 0 },
+          ],
+        },
+        {
+          exerciseName: "Lat Pulldown",
+          sets: [{ setNumber: 1, weight: 0, unit: "lbs", reps: 0 }],
+        },
+      ],
+      isTemplate: true,
+    },
+    {
+      date: "2024-05-31T20:04:02.485Z",
+      name: "Back Day",
+      exercises: [
+        {
+          exerciseName: "Walking Lunge",
+          sets: [
+            { setNumber: 1, weight: 3, unit: "lbs", reps: 8 },
+            { setNumber: 2, weight: 2, unit: "lbs", reps: 4 },
+          ],
+        },
+        {
+          exerciseName: "Lat Pulldown",
+          sets: [{ setNumber: 1, weight: 1, unit: "lbs", reps: 12 }],
+        },
+        {
+          exerciseName: "Lat Pulldown",
+          sets: [{ setNumber: 1, weight: 1, unit: "lbs", reps: 12 }],
+        },
+        {
+          exerciseName: "Lat Pulldown",
+          sets: [{ setNumber: 1, weight: 1, unit: "lbs", reps: 12 }],
+        },
+        {
+          exerciseName: "Lat Pulldown",
+          sets: [{ setNumber: 1, weight: 1, unit: "lbs", reps: 12 }],
+        },
+      ],
+      isTemplate: true,
+    },
+    {
+      date: "2024-05-31T20:04:02.485Z",
+      name: "Template 2",
+      exercises: [
+        {
+          exerciseName: "Walking Lunge",
+          sets: [
+            { setNumber: 1, weight: 3, unit: "lbs", reps: 8 },
+            { setNumber: 2, weight: 2, unit: "lbs", reps: 4 },
+          ],
+        },
+        {
+          exerciseName: "Lat Pulldown",
+          sets: [{ setNumber: 1, weight: 1, unit: "lbs", reps: 12 }],
+        },
+      ],
+      isTemplate: true,
+    },
+    {
+      date: "2024-05-31T20:04:02.485Z",
+      name: "Shoulders",
+      exercises: [
+        {
+          exerciseName: "Walking Lunge",
+          sets: [
+            { setNumber: 1, weight: 3, unit: "lbs", reps: 8 },
+            { setNumber: 2, weight: 2, unit: "lbs", reps: 4 },
+          ],
+        },
+        {
+          exerciseName: "Lat Pulldown",
+          sets: [{ setNumber: 1, weight: 1, unit: "lbs", reps: 12 }],
+        },
+      ],
+      isTemplate: true,
+    },
+  ],
+};
+
+const MyTemplates: React.FC<MyTemplatesProps> = ({}) => {
+  const templates = mockData.logs.filter((log) => log.isTemplate);
+  //   const templates = null; Case for no template
+  return (
+    <div>
+      <div className="flex flex-col bg-white p-10 rounded-xl relative justify-evenly items-center text-center">
+        <h1 className="text-4xl font-bold text-center">
+          Choose From Templates
+        </h1>
+        {templates ? (
+          <h1>Please choose one template to start:</h1>
+        ) : (
+          <h1> There are currently no templates. Create one below</h1>
+        )}
+
+        {/* Templates to Choose From */}
+        <div className="flex flex-wrap -mx-2">
+          {templates &&
+            templates.map((template, index) => (
+              <div key={index} className="w-full sm:w-1/2 lg:w-1/3 px-2 mb-4">
+                <TemplateCard data={template} />
+              </div>
+            ))}
+        </div>
+        <div className="flex flex-col justify-between h-28">
+          <BasicRoundedButton label="Create Template" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyTemplates;
---
+++ src/components/cards/TemplateCard.tsx
@@ -0,0 +1,32 @@
+import { Log } from "@/models/log.model";
+import React from "react";
+
+interface TemplateCardProps {
+  data: Log;
+  onClick: () => void;
+}
+
+const TemplateCard: React.FC<TemplateCardProps> = ({ data, onClick }) => {
+  const exercisesToShow = data.exercises.slice(0, 5);
+
+  return (
+    <div
+      className="border border-gray-300 shadow-md rounded-lg m-2 bg-white cursor-pointer min-h-[240px] hover:border-orange-500"
+      onClick={onClick}
+    >
+      <div className="font-bold text-lg mb-2 bg-orange-500 p-2 rounded-t-lg text-white">
+        {data.name}
+      </div>
+      <div className="p-4">
+        {exercisesToShow.map((exercise, index) => (
+          <div className="flex justify-between items-center mb-1" key={index}>
+            <div className="text-gray-700">{exercise.exerciseName}</div>
+            <div className="text-gray-500">Sets: {exercise.sets.length}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TemplateCard;
