2f0faf705fb833ca0fc8513e41448b273d700c57
David Eastmond <davebuck1981@gmail.com>
2024-06-04 14:01:52 -0400

    fix view logs comments

--- src/app/user/viewlogs/page.tsx
+++ src/app/user/viewlogs/page.tsx
@@ -1,17 +1,10 @@
 "use client";
+import { CalendarLogViewer } from "@/components/calendar-log-viewer/Calendar-log-viewer";
 import { useAuthSession } from "@/lib/contexts/auth-context/auth-context";
 import { useRouter } from "next/navigation";
-import { CalendarLogViewer } from "@/components/calendar-log-viewer/Calendar-log-viewer";
-
 
 export default function ViewLogs() {
-
-  /*
-    This is an example of how to use the hook to access the auth sesion, which indicates if user is logged in or not.
-    It also has basic user data that we may need to use on the front end (like the email, username, _id).
-    We can always add more data to the session object in the auth-options.ts file.
-  */
-  const { status, session } = useAuthSession(); // status, session and update are available, see auth-context.tsx
+  const { status } = useAuthSession(); // status, session and update are available, see auth-context.tsx
   const router = useRouter();
 
   // Users who are not authenticated will be redirected to the sign in page.
@@ -23,5 +16,5 @@ export default function ViewLogs() {
     <div>
       <CalendarLogViewer />
     </div>
-  )
+  );
 }
