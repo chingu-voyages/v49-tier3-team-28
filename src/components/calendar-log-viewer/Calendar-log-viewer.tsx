79f22f2b30f6dcb0ac990b7beb91f9ff6fd88c17
David Eastmond <davebuck1981@gmail.com>
2024-06-04 13:59:50 -0400

    Finished calendar log

--- src/components/calendar-log-viewer/Calendar-log-viewer.tsx
+++ src/components/calendar-log-viewer/Calendar-log-viewer.tsx
@@ -1,6 +1,7 @@
 import { LoggingClient } from "@/app/clients/logging-client/logging-client";
+import { ExerciseActivity } from "@/models/exercise-activity.model";
 import { Log } from "@/models/log.model";
-import { Alert } from "@mui/material";
+import { Alert, CircularProgress } from "@mui/material";
 import {
   DateCalendar,
   LocalizationProvider,
@@ -11,38 +12,50 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs, { Dayjs } from "dayjs";
 import { useEffect, useState } from "react";
 
-// Goals for functionality:
-/* 
-1. Future date is disabled.
-2. How far back do we allow the user to go?
-*/
-
 export function CalendarLogViewer() {
   const [currentLogsByMonthAndYear, setCurrentLogsByMonthAndYear] = useState<
     Log[]
-  >([]); // Store the logs for the current month and year.
+  >([]);
+
   const [highlightedDays, setHighlightedDays] = useState<number[]>([]); // The days that will be highlighted in the calendar.
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [value, setValue] = useState<Dayjs | null>(dayjs());
+
   useEffect(() => {
-    // When the component mounts, we need to fetch the logs for the current month and year.
-    // This is assuming that when the calendar component mounts, it will be the current month and year.
-    fetchLogsByMonthAndYear(new Date().getMonth(), new Date().getFullYear());
+    // When the calendar mounts, we need to render the highlights for the current month
+    // And if there are any logs for the current date, load them
+    const fetchLogsForCurrentDate = async () => {
+      try {
+        setIsLoading(true);
+        const apiDataResponse = await LoggingClient.getLogsByMonthAndYear(
+          dayjs().month(),
+          dayjs().year()
+        );
+        setCurrentLogsByMonthAndYear(apiDataResponse.logs);
+        setHighlightedDays(extractHighligtedDaysFromLogs(apiDataResponse.logs));
+      } catch (e: any) {
+        console.log(e.message);
+        setErrorMessage(e.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchLogsForCurrentDate();
   }, []);
 
-  const fetchLogsByMonthAndYear = async (month: number, year: number) => {
-    // When the month changes, we need to fetch the logs for that month and year.
+  const handleMonthChange = async (date: Dayjs) => {
+    // When the month changes, we need to fetch the logs for that month and year to highlight the days in calendar
     try {
       setIsLoading(true);
       const apiDataResponse = await LoggingClient.getLogsByMonthAndYear(
-        month,
-        year
+        date.month(),
+        date.year()
       );
 
-      setCurrentLogsByMonthAndYear(apiDataResponse.logs);
       setHighlightedDays(extractHighligtedDaysFromLogs(apiDataResponse.logs));
     } catch (e: any) {
-      // TODO: Do something with the error.
       console.log(e.message);
       setErrorMessage(e.message);
     } finally {
@@ -50,69 +63,90 @@ export function CalendarLogViewer() {
     }
   };
 
-  const handleMonthChange = async (date: Dayjs) => {
-    // When the month changes, we need to fetch the logs for that month and year.
-    await fetchLogsByMonthAndYear(date.month(), date.year());
+  const handleCalendarDateChange = async (date: Dayjs) => {
+    // When the user selects a date, we need to fetch the logs for that date.
+    setValue(date);
+    const apiResponse = await LoggingClient.getLogsByMonthAndYear(
+      date.month(),
+      date.year()
+    );
+    setCurrentLogsByMonthAndYear(apiResponse.logs);
   };
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar
-        disableFuture
-        disabled={isLoading}
-        slots={{
-          day: (
-            props: PickersDayProps<Dayjs> & { highlightedDays?: number[] }
-          ) => {
-            const {
-              highlightedDays = [],
-              day,
-              outsideCurrentMonth,
-              ...other
-            } = props;
-            const isSelected =
-              !props.outsideCurrentMonth &&
-              highlightedDays.indexOf(props.day.date()) >= 0;
-            return (
-              <PickersDay
-                {...other}
-                day={day}
-                outsideCurrentMonth={outsideCurrentMonth}
-                sx={{
-                  backgroundColor: isSelected ? "#2F4858" : "inherit",
-                  color: isSelected ? "white" : "inherit",
-                  fontWeight: 700,
-                }}
-              />
-            );
-          },
-        }}
-        slotProps={{
-          day: {
-            highlightedDays: highlightedDays,
-          } as any,
-        }}
-        sx={{
-          "& .MuiPickersCalendarHeader-label": {
-            backgroundColor: "#03BB9B",
-            borderRadius: "20px",
-            paddingLeft: "10px",
-            paddingRight: "10px",
-            fontWeight: 700,
-            color: "white",
-          },
-          "& .MuiPickersCalendarHeader-switchViewIcon": {
-            display: "none",
-          },
-        }}
-        onMonthChange={handleMonthChange}
-      />
+      <div>
+        {isLoading && (
+          <div className="absolute left-1/2 inset-y-1/4">
+            <CircularProgress />
+          </div>
+        )}
+        <DateCalendar
+          disableFuture
+          disabled={isLoading}
+          value={value}
+          onChange={handleCalendarDateChange}
+          slots={{
+            day: (
+              props: PickersDayProps<Dayjs> & { highlightedDays?: number[] }
+            ) => {
+              const {
+                highlightedDays = [],
+                day,
+                outsideCurrentMonth,
+                ...other
+              } = props;
+              const isSelected =
+                !props.outsideCurrentMonth &&
+                highlightedDays.indexOf(props.day.date()) >= 0;
+              return (
+                <PickersDay
+                  {...other}
+                  day={day}
+                  outsideCurrentMonth={outsideCurrentMonth}
+                  sx={{
+                    backgroundColor: isSelected ? "#2F4858" : "inherit",
+                    color: isSelected ? "white" : "inherit",
+                    fontWeight: 700,
+                  }}
+                />
+              );
+            },
+          }}
+          slotProps={{
+            day: {
+              highlightedDays: highlightedDays,
+            } as any,
+          }}
+          sx={{
+            "& .MuiPickersCalendarHeader-label": {
+              backgroundColor: "#03BB9B",
+              borderRadius: "20px",
+              paddingLeft: "10px",
+              paddingRight: "10px",
+              fontWeight: 700,
+              color: "white",
+            },
+            "& .MuiPickersCalendarHeader-switchViewIcon": {
+              display: "none",
+            },
+          }}
+          onMonthChange={handleMonthChange}
+        />
+      </div>
       <div>
         {errorMessage && (
           <Alert severity="error" variant="outlined" sx={{ marginTop: "10px" }}>
             {errorMessage}
           </Alert>
         )}
+      </div>
+      <div>
+        <h4 className="text-center">{value?.format("ddd MMMM D, YYYY")}</h4>
+      </div>
+      <div>
+        {/* Logs for a selected date appear here */}
+        {renderLogsForDay(value, currentLogsByMonthAndYear)}
       </div>
     </LocalizationProvider>
   );
@@ -121,3 +155,68 @@ export function CalendarLogViewer() {
 function extractHighligtedDaysFromLogs(logs: Log[]): number[] {
   return logs.map((log) => dayjs(log.createdAt).date());
 }
+
+function renderLogsForDay(
+  dateData: Dayjs | null,
+  logs: Log[]
+): JSX.Element[] | null {
+  // Logs should be the current month's logs stored in the state
+  // we use the exercise table in read-only mode
+  if (dateData === null) return null;
+
+  const renderedLogData = logs
+    .filter((log) => dayjs(log.createdAt).date() === dateData.date())
+    .map((log) => {
+      return renderExerciseTables(log.exercises);
+    });
+
+  return renderedLogData.length > 0
+    ? renderedLogData
+    : [<p className="text-center transition-opacity">No logs for this date</p>];
+}
+
+const renderExerciseTables = (ex: ExerciseActivity[]) => {
+  return (
+    <div>
+      {ex.map((exercise, index) => {
+        return (
+          <>
+            <div>
+              <h4 className="text-white text-sm font-bold p-2 defaultButtonColor">
+                {exercise.exerciseName}
+              </h4>
+            </div>
+            <table className="w-full border-collapse bg-white">
+              <tbody>
+                {exercise.sets.map((set, setIndex) => (
+                  <tr
+                    key={setIndex}
+                    className="odd:bg-orange-100 text-center text-sm"
+                  >
+                    <td className="p-2 text-center">{setIndex + 1}</td>
+                    <td className="p-2">
+                      <div className="flex">
+                        <p>{set.weight}</p>
+
+                        <div>
+                          <p className="px-2 self-center">{set.unit}</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="p-2 text-center ">
+                      <div className="flex">
+                        <p>{set.reps}</p>
+
+                        <p className="px-2 self-center">reps</p>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        );
+      })}
+    </div>
+  );
+};
