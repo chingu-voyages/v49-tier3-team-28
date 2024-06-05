5c5fe342bb2664d1773c1e5705e3889258828fb3
David Eastmond <davebuck1981@gmail.com>
2024-06-05 19:59:49 -0400

    style refactors

--- src/components/modals/TemplateDataModal.tsx
+++ src/components/modals/TemplateDataModal.tsx
@@ -8,7 +8,7 @@ import { BasicRoundedButton } from "../buttons/basic-rounded-button/Basic-rounde
 interface TemplateDataModalProps {
   open: boolean;
   onClose: () => void;
-  exerciseData: ExerciseActivity[] | null;
+  exerciseData: ExerciseActivity[] | null; // TODO: I think we should be passing a log instead of exercise activities here
 }
 
 const TemplateDataModal: React.FC<TemplateDataModalProps> = ({
@@ -25,48 +25,56 @@ const TemplateDataModal: React.FC<TemplateDataModalProps> = ({
   };
 
   return (
-    <Modal
-      open={open}
-      onClose={onClose}
-      className="flex justify-center items-center"
-    >
-      <div className="flex flex-col w-1/2 h-3/4 bg-white p-10 rounded-xl relative justify-evenly items-center text-center">
+    <Modal open={open} onClose={onClose} className="p-4">
+      <div className="flex flex-col w-1/2 h-3/4 p-4 bg-white rounded-xl relative justify-evenly w-full">
         {/* Templates to Choose From */}
         {exerciseData !== null ? (
           <div className="mt-4 w-full">
-            <button className="absolute top-2 right-2" onClick={onClose}>
-              <FiX />
-            </button>
-
-            <h2 className="text-2xl font-semibold mb-4">Exercise List</h2>
-
+            <div className="flex justify-between mb-4">
+              {/* TODO: For Exercise list we should have access to the log.name (template Name) */}
+              <h2 className="futuraFont text-2xl leading-7 font-medium">
+                Exercise List
+              </h2>
+              <button onClick={onClose}>
+                <FiX className="size-5" />
+              </button>
+            </div>
             {/* Exercise Activity */}
             <div className="min-h-96 max-h-96 overflow-y-auto overflow-hidden">
-              <div className="rounded-xl mb-4 border border-gray-100 shadow-md relative">
-                <table className="w-full border-collapse bg-white">
-                  <thead>
-                    <tr className="text-white text-center bg-orange-500 p-2">
-                      <th className="p-left-2">Exercise Name</th>
-                      <th className="p-left-2"># of Sets</th>
-                    </tr>
-                  </thead>
-                  {exerciseData.map((exercise, eIdx) => (
-                    <tbody>
-                      <td className="p-2 text-center">
-                        {exercise.exerciseName}
-                      </td>
-                      <td className="p-2 text-center">
-                        {exercise.sets.length}
-                      </td>
-                    </tbody>
-                  ))}
-                </table>
-              </div>
+              {exerciseData.map((exercise, eIdx) => (
+                <div className="rounded-xl mb-4 border border-gray-100 shadow-md relative">
+                  <h4 className="text-white font-bold p-2 defaultButtonColor">
+                    {exercise.exerciseName}
+                  </h4>
+                  <table className="w-full border-collapse bg-white">
+                    {exercise.sets.map((set, eIdx) => (
+                      <tbody>
+                        <td className="p-2">
+                          <b>{set.setNumber}</b> set
+                        </td>
+                        <td className="p-2">
+                          <b>{set.weight}</b> {set.unit}{" "}
+                        </td>
+                        <td className="p-2 text-right">
+                          <b>{set.reps}</b> reps
+                        </td>
+                      </tbody>
+                    ))}
+                  </table>
+                </div>
+              ))}
             </div>
           </div>
         ) : null}
-        <div className="flex flex-col justify-between h-28 mt-4">
-          <BasicRoundedButton onClick={onClose} label="Back to Selection" />
+        <div className="flex flex-col justify-between h-28 mt-4 w-full">
+          <BasicRoundedButton
+            onClick={onClose}
+            label="Back to Selection"
+            buttonClassNames="!w-full"
+            customMaterialButtonStyles={{
+              backgroundColor: "#95A1A8",
+            }}
+          />
           {pathname == "/user/mytemplates" && (
             <BasicRoundedButton
               onClick={() => handleUseTemplate(exerciseData!)}
