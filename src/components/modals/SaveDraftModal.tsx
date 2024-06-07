9a807f14f37e04bbbd6301df6f5a53e370dfc775
Tandid Alam <tandid.alam@gmail.com>
2024-06-07 11:00:58 -0400

    Fixed props for draft

--- src/components/modals/SaveDraftModal.tsx
+++ src/components/modals/SaveDraftModal.tsx
@@ -1,36 +1,24 @@
-import { Box, Button, Modal, Typography } from "@mui/material";
+import { Modal } from "@mui/material";
 
-const SaveDraftModal = ({ open, onClose, onSaveDraft }) => {
+interface SaveDraftModalProps {
+  open: boolean;
+  onClose: () => void;
+  onSaveDraft: () => void;
+}
+
+const SaveDraftModal: React.FC<SaveDraftModalProps> = ({
+  open,
+  onClose,
+  onSaveDraft,
+}) => {
   return (
-    <Modal open={open} onClose={onClose}>
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: 300,
-          bgcolor: "background.paper",
-          boxShadow: 24,
-          p: 4,
-          borderRadius: 2,
-        }}
-      >
-        <Typography variant="h6" component="h2">
-          Save as Draft?
-        </Typography>
-        <Typography sx={{ mt: 2 }}>
-          Do you want to save your current log as a draft before exiting?
-        </Typography>
-        <Box sx={{ mt: 4, display: "flex", justifyContent: "space-between" }}>
-          <Button variant="contained" color="primary" onClick={onSaveDraft}>
-            Yes
-          </Button>
-          <Button variant="outlined" color="secondary" onClick={onClose}>
-            No
-          </Button>
-        </Box>
-      </Box>
+    <Modal open={open} onClose={onClose} className="min-w-screen">
+      <div className="bg-white w-1/2 flex flex-col items-center">
+        <h2>Save as Draft?</h2>
+        <h2>Do you want to save your current log as a draft before exiting?</h2>
+        <button onClick={onSaveDraft}>Yes</button>
+        <button onClick={onClose}>No</button>
+      </div>
     </Modal>
   );
 };
