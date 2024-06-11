5c819a7ac72bac57c917393381ff91c24a25df46
Tandid Alam <tandid.alam@gmail.com>
2024-06-11 15:52:59 -0400

    added animation to Exercise Table

--- src/components/tables/ExerciseTable.tsx
+++ src/components/tables/ExerciseTable.tsx
@@ -1,4 +1,5 @@
 import { Set } from "@/models/set.model";
+import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 import { FiTrash, FiX } from "react-icons/fi";
 
@@ -21,32 +22,36 @@ const ExerciseTable: React.FC<ExerciseTableProps> = ({
   onDeleteExercise,
   idx,
 }) => {
-
-
   const handleInputChange = (
     setIndex: number,
     field: keyof Set,
     e: React.ChangeEvent<HTMLInputElement>
   ) => {
-
-    const newValue = parseFloat(e.target.value)
+    const newValue = parseFloat(e.target.value);
 
     onSetChange(setIndex, field, newValue);
   };
 
-  const handleBlur = (setIndex: number, field: keyof Set, e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleBlur = (
+    setIndex: number,
+    field: keyof Set,
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const value = parseFloat(e.target.value.replace(/^0+/, "")) || 0;
 
-    const value = parseFloat(e.target.value.replace(/^0+/, "")) || 0
-
-    onSetChange(setIndex, field, value)
-  }
-
+    onSetChange(setIndex, field, value);
+  };
 
   return (
     <>
-      <h4 className="text-white text-sm font-bold p-2 defaultButtonColor">
+      <motion.h4
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="text-white text-sm font-bold p-2 defaultButtonColor"
+      >
         {exerciseName}
-      </h4>
+      </motion.h4>
 
       <button
         onClick={() => onDeleteExercise(idx)}
@@ -54,57 +59,68 @@ const ExerciseTable: React.FC<ExerciseTableProps> = ({
       >
         <FiX />
       </button>
-      <table className="w-full border-collapse bg-white">
+      <motion.table
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="w-full border-collapse bg-white"
+      >
         <tbody>
-          {sets.map((set, setIndex) => (
-            <tr
-              key={setIndex}
-              className="odd:bg-orange-100 text-center text-sm"
-            >
-              <td className="p-2 text-center">{setIndex + 1}</td>
+          <AnimatePresence>
+            {sets.map((set, setIndex) => (
+              <motion.tr
+                key={setIndex}
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 10 }}
+                transition={{ duration: 0.3 }}
+                className="odd:bg-orange-100 text-center text-sm"
+              >
+                <td className="p-2 text-center">{setIndex + 1}</td>
 
-              <td className="p-2">
-                <div className="flex">
-                  <input
-                    type="number"
-                    value={set.weight.toString()}
-                    onChange={(e) => handleInputChange(setIndex, "weight", e)}
-                    className="w-20 p-1 border rounded-xl text-center bg-gray-50"
-                    placeholder="0"
-                    onBlur={(e) => handleBlur(setIndex, "weight", e)}
-                  />
-                  <div className="self-center">
-                    <p className="px-2">{unit}</p>
+                <td className="p-2">
+                  <div className="flex">
+                    <input
+                      type="number"
+                      value={set.weight.toString()}
+                      onChange={(e) => handleInputChange(setIndex, "weight", e)}
+                      className="w-20 p-1 border rounded-xl text-center bg-gray-50"
+                      placeholder="0"
+                      onBlur={(e) => handleBlur(setIndex, "weight", e)}
+                    />
+                    <div className="self-center">
+                      <p className="px-2">{unit}</p>
+                    </div>
                   </div>
-                </div>
-              </td>
-              <td className="p-2 text-center ">
-                <div className="flex">
-                  <input
-                    type="number"
-                    value={set.reps.toString()}
-                    onChange={(e) => handleInputChange(setIndex, "reps", e)}
-                    className="w-20 p-1 border rounded-xl text-center bg-gray-50"
-                    placeholder="0"
-                    onBlur={(e) => handleBlur(setIndex, "reps", e)}
-                  />
-                  <p className="px-2 self-center">reps</p>
-                </div>
-              </td>
-              <td className="p-1">
-                {sets.length > 1 && (
-                  <button
-                    onClick={() => onDeleteSet(setIndex)}
-                    className=" text-red-500"
-                  >
-                    <FiTrash />
-                  </button>
-                )}
-              </td>
-            </tr>
-          ))}
+                </td>
+                <td className="p-2 text-center ">
+                  <div className="flex">
+                    <input
+                      type="number"
+                      value={set.reps.toString()}
+                      onChange={(e) => handleInputChange(setIndex, "reps", e)}
+                      className="w-20 p-1 border rounded-xl text-center bg-gray-50"
+                      placeholder="0"
+                      onBlur={(e) => handleBlur(setIndex, "reps", e)}
+                    />
+                    <p className="px-2 self-center">reps</p>
+                  </div>
+                </td>
+                <td className="p-1">
+                  {sets.length > 1 && (
+                    <button
+                      onClick={() => onDeleteSet(setIndex)}
+                      className="text-red-500"
+                    >
+                      <FiTrash />
+                    </button>
+                  )}
+                </td>
+              </motion.tr>
+            ))}
+          </AnimatePresence>
         </tbody>
-      </table>
+      </motion.table>
     </>
   );
 };
