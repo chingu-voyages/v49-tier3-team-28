a3f5ed734fef97d44fbbf689cf24b39d429e0341
Tandid Alam <tandid.alam@gmail.com>
2024-06-11 01:15:27 -0400

    Updated CSS for SuccessModal

--- src/components/modals/SuccessModal.tsx
+++ src/components/modals/SuccessModal.tsx
@@ -2,6 +2,7 @@ import { Modal } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { BasicRoundedButton } from "../buttons/basic-rounded-button/Basic-rounded-button";
 
 interface SuccessModalProps {
   open: boolean;
@@ -10,7 +11,7 @@ interface SuccessModalProps {
 const SuccessModal: React.FC<SuccessModalProps> = ({ open }) => {
   return (
     <Modal open={open}>
-      <div className="flex flex-col gap-4 bg-gray-100 p-6 rounded-3xl relative justify-evenly h-fill-available ml-4 mr-4 mt-10 mb-10">
+      <div className="flex flex-col gap-4 modalBgColor p-6 rounded-3xl relative justify-evenly h-fill-available ml-4 mr-4 mt-10 mb-10">
         <div className="flex justify-center">
           <Image
             src="/success.svg"
@@ -20,23 +21,27 @@ const SuccessModal: React.FC<SuccessModalProps> = ({ open }) => {
           />
         </div>
         <div>
-          <h1 className="text-2xl verdanaFont text-center">SUCCESS!</h1>
-          <h3 className="robotoFont text-sm text-center mb-4">
-            Your exercise template has been saved successfully. You can now
-            re-use this template to log your exercises next time!
-          </h3>
-          <div className="mt-2 flex flex-col gap-4 px-4">
-            <Link
-              className="rounded-3xl bg-slate-500 p-2 text-white font-semibold text-center hover:bg-slate-600"
-              href={"/user/home"}
-            >
-              Return to Home
+          <div className="flex flex-col gap-">
+            <h1 className="text-xl verdanaFont text-center darkCharcoal">
+              SUCCESS!
+            </h1>
+            <h3 className="robotoFont text-sm m-6 text-gray-500">
+              Your exercise template has been saved successfully. You can now
+              re-use this template to log your exercises next time!
+            </h3>
+          </div>
+          <div className="mt-2 flex flex-col gap-4 px-4 items-center">
+            <Link href={"/user/home"}>
+              <BasicRoundedButton
+                label="Return To Home"
+                buttonClassNames="secondaryButtonColor"
+              />
             </Link>
-            <Link
-              className="rounded-3xl bg-orange-500 p-2 text-white font-semibold text-center hover:bg-orange-600"
-              href={"/user/mytemplates"}
-            >
-              View Templates
+            <Link href={"/user/mytemplates"}>
+              <BasicRoundedButton
+                label="View Templates"
+                buttonClassNames="defaultButtonColor"
+              />
             </Link>
           </div>
         </div>
