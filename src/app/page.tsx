2928a02a1a6de37c0f3a6fdbafff4894dc4099c9
Tandid Alam <tandid.alam@gmail.com>
2024-06-11 01:47:17 -0400

    setup framer motion, home page button animation added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-// This is the title page of the application "/
+"use client";
+
 import { AppNameTextLogo } from "@/components/App-name-text-logo/App-name-text-logo";
 import { BasicRoundedButton } from "@/components/buttons/basic-rounded-button/Basic-rounded-button";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import BackgroundTitleSplash from "../../public/images/title-page/title-page-splash.png";
 import moduleStyles from "./home-page.module.css";
@@ -15,24 +17,37 @@ export default function TitlePage() {
     >
       <AppNameTextLogo />
       {/* Signup, signin buttons */}
-      <div className="flex flex-col gap-y-9">
-        <div>
+      <motion.div
+        className="flex flex-col gap-y-9"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+      >
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+        >
           <Link href="/signup">
             <BasicRoundedButton
               label="Sign up"
               buttonClassNames={`${moduleStyles.whiteSignUpButton}`}
             />
           </Link>
-        </div>
-        <div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 1 }}
+        >
           <Link href="/signin">
             <BasicRoundedButton
               label="Sign in"
               buttonClassNames={moduleStyles.signInButton}
             />
           </Link>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
