4c5a8781102c8d672b406227936d3548890b01bb
Tandid Alam <tandid.alam@gmail.com>
2024-06-11 16:51:31 -0400

    added animation for saving draft and continuing, also cleaned up code

--- src/components/modals/SaveDraftModal.tsx
+++ src/components/modals/SaveDraftModal.tsx
@@ -1,4 +1,5 @@
 import { Modal } from "@mui/material";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import React from "react";
 import { FiX } from "react-icons/fi";
@@ -16,43 +17,46 @@ const SaveDraftModal: React.FC<SaveDraftModalProps> = ({
   onSaveDraft,
 }) => {
   return (
-    <Modal open={open} onClose={onClose}>
-      <div className="bg-white fixed inset-0 flex items-center justify-center bg-opacity-70">
-        <div className="m-4 pt-8 pb-8 mb-80 modalBgColor rounded-3xl w-full max-w-md shadow-lg text-center relative max-h-min">
-          <button
-            onClick={onClose}
-            className="absolute top-8 right-6 scale-150 darkCharcoal"
-          >
-            <FiX />
-          </button>
-          <h1 className="text-xl verdanaFont ml-10 text-left darkCharcoal ">
-            Are You Sure?
-          </h1>
-          <p className="robotoFont text-sm m-10 text-gray-500 text-left">
-            You have unsaved exercise data. Do you want to save it before
-            leaving?
-          </p>
-          <div className="flex justify-between flex-col items-center gap-4">
-            <BasicRoundedButton
-              onClick={onSaveDraft}
-              label="Save and Exit"
-              buttonClassNames="defaultButtonColor"
-            />
+    <Modal open={open} onClose={onClose} className="modalOuterContainer">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.8 }}
+        transition={{ duration: 0.3 }}
+        className="modalInnerContainer modalBgColor shadow-lg relative"
+      >
+        <button
+          onClick={onClose}
+          className="absolute top-8 right-6 scale-150 darkCharcoal"
+        >
+          <FiX />
+        </button>
+        <h1 className="text-xl verdanaFont ml-10 text-left darkCharcoal ">
+          Are You Sure?
+        </h1>
+        <p className="robotoFont text-sm m-10 text-gray-500 text-left">
+          You have unsaved exercise data. Do you want to save it before leaving?
+        </p>
+        <div className="flex justify-between flex-col items-center gap-4">
+          <BasicRoundedButton
+            onClick={onSaveDraft}
+            label="Save and Exit"
+            buttonClassNames="defaultButtonColor"
+          />
 
-            <Link href="/user/home">
-              <BasicRoundedButton
-                onClick={() => {
-                  if (localStorage.getItem("draft")) {
-                    localStorage.removeItem("draft");
-                  }
-                }}
-                label="Leave Without Saving"
-                buttonClassNames="secondaryButtonColor"
-              />
-            </Link>
-          </div>
+          <Link href="/user/home">
+            <BasicRoundedButton
+              onClick={() => {
+                if (localStorage.getItem("draft")) {
+                  localStorage.removeItem("draft");
+                }
+              }}
+              label="Leave Without Saving"
+              buttonClassNames="secondaryButtonColor"
+            />
+          </Link>
         </div>
-      </div>
+      </motion.div>
     </Modal>
   );
 };
